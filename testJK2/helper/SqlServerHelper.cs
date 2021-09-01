cdd5f4752f012c256a76f589a500528b310a9473
wjz <279646436@qq.com>
2021-09-01 23:27:55 +0800

    修改IP配置

--- testJK2/helper/SqlServerHelper.cs
+++ testJK2/helper/SqlServerHelper.cs
@@ -10,7 +10,8 @@ namespace testJK2.helper
     {
         public static SqlConnection getSqlServerConnection() {
             //http://192.168.12.53:9012/TestWebService.asmx/add?x=10&y=12
-            string dbIP = "192.168.12.53";
+            //string dbIP = "192.168.12.53";
+            string dbIP = "192.168.3.183";
             string dbName = "cmis20190904230604";
             string dbUserName = "sa";
             string dbPwd = "abc123";
