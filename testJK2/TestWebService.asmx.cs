8f6c375f2f8a9a78e4ee5abcdd5120d8ec702665
wjz <279646436@qq.com>
2021-09-02 16:02:41 +0800

    更改数据库

--- testJK2/TestWebService.asmx.cs
+++ testJK2/TestWebService.asmx.cs
@@ -49,6 +49,14 @@ namespace testJK2
             return getJsonResult(loginServie.getTestData());
         }
 
+        [WebMethod]//获取数据库
+        public string getBaseDatabase()
+        {
+            LoginService loginServie = new LoginService();
+            return getJsonResult(loginServie.getTestData());
+        }
+
+
         private string getJsonResult(string result)
         {
             Context.Response.Charset = "GB2312"; //设置字符集类型  
