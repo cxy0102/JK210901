ae328ea0e0dbcdbb0f3aeb13abd3e86194221305
wjz <279646436@qq.com>
2021-09-02 16:12:25 +0800

    init:搭建获取干部信息结构

---
+++ testJK2/dao/impl/CadreinfoDaoImpl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testJK2.dao.impl
+{
+    public class CadreInfoDaoImpl : CadreInfoDao
+    {
+        public string getDataBase()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ testJK2/service/CadreInfoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using testJK2.dao;
+using testJK2.dao.impl;
+
+namespace testJK2.service
+{
+    public class CadreInfoService : CadreInfoDao
+    {
+        public string getDataBase()
+        {
+            CadreInfoDao cadreDao = new CadreInfoDaoImpl();
+            return cadreDao.getDataBase();
+        }
+    }
+}
